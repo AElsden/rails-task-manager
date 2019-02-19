1db6013611a6ab2931482ded4038cd162b1d13e5
Alex Elsden <alex.elsden@hotmail.co.uk>
2019-02-19 12:37:58 +0000

    added routes, migrated new table, added added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # read all
+  get 'tasks', to: 'tasks#index'
+
+  # read for one
+  get 'tasks', to: 'tasks#show'
+
+  #create new
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  #update record
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/id', to: 'tasks#update'
+
+  #delete record
+  delete 'restaurants/:id', to: 'restaurants#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
