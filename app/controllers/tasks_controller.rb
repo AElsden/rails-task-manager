e4aee3afc371d3c038253d249e5a8350a76021b9
Alex Elsden <alex.elsden@hotmail.co.uk>
2019-02-19 16:12:18 +0000

    completed task updater

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -16,4 +17,28 @@ class TasksController < ApplicationController
     Task.create(task_params)
     redirect_to tasks_path
   end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,15 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   #create new
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
   post 'tasks', to: 'tasks#create'
 
   # read for one
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-
   #update record
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
 
   #delete record
   delete 'restaurants/:id', to: 'restaurants#destroy'
